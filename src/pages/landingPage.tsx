3d590a48fde54e4b24fc0680eae4faaca4263641
BeTardif <157613113+BeTardif@users.noreply.github.com>
2026-06-27 18:18:37 -0400

    4 dropdowns in landing page

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -5,7 +5,42 @@ class LandingPage extends Component{
     render(){
         return(
             <div id = "langingPage">
-                something
+                <div className="landingFilters">
+                    <label className="landingFilter">
+                        <span className="landingFilterLabel">Marchandise</span>
+                        <select className="landingFilterSelect" defaultValue="">
+                            <option value="" disabled>Sélectionner…</option>
+                        </select>
+                    </label>
+
+                    <label className="landingFilter">
+                        <span className="landingFilterLabel">Pays Exportateur</span>
+                        <select className="landingFilterSelect" defaultValue="">
+                            <option value="" disabled>Sélectionner…</option>
+                        </select>
+                    </label>
+
+                    <label className="landingFilter">
+                        <span className="landingFilterLabel">Pays Importateur</span>
+                        <select className="landingFilterSelect" defaultValue="">
+                            <option value="" disabled>Sélectionner…</option>
+                        </select>
+                    </label>
+
+                    <label className="landingFilter">
+                        <span className="landingFilterLabel">Incoterm</span>
+                        <select className="landingFilterSelect" defaultValue="">
+                            <option value="" disabled>Sélectionner…</option>
+                        </select>
+                    </label>
+                </div>
+
+                <button
+                    className="landingSimulateButton"
+                    onClick={() => alert("Rien n'est programmé")}
+                >
+                    Simuler
+                </button>
             </div>
         )
     }
